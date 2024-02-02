90513dc1f7bb198947b32c7b24854b8d2e93c0fb
Evan Lee <evan.lee.615@gmail.com>
2024-02-02 00:03:19 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+let feet;
+function scuberGreetingForFeet(feet){
+  if(feet > 2500) {
+    return 'No can do.';
+  } else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 400) {
+    return 'That will be twenty bucks.';
+  } else if (feet <= 400) {    
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+let city;
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.': 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+let tip;
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
